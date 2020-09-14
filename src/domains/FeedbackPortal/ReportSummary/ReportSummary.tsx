8cbeb6b89aeaa9f67aa2f59c86f223d45d9fd464
Johan Guzman <jguzm022@ucr.edu>
2020-09-14 13:36:48 -0700

    Adds resolved and reply components

--- src/domains/FeedbackPortal/ReportSummary/ReportSummary.tsx
+++ src/domains/FeedbackPortal/ReportSummary/ReportSummary.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Collapse from '@material-ui/core/Collapse';
 import Grid from '@material-ui/core/Grid';
 import { Delete as DeleteIcon } from '@material-ui/icons';
+import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { red, green, yellow } from '@material-ui/core/colors';
@@ -14,15 +15,24 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ResolvedIcon from '@material-ui/icons/Done';
 import UnresolvedIcon from '@material-ui/icons/Warning';
+import ReplyIcon from '@material-ui/icons/Comment';
 
 import { formatDate } from 'utils/format';
 import Bold from 'components/Bold';
+import Loader from 'components/Loader';
+import Dialog from 'components/Dialog';
+import TextField from 'components/TextField';
 import ReportStateContext from '../Contexts/ReportStateContext';
 import Reply from '../Reply';
 import FormBase from '../FormBase';
 import { FeedbackReport, BugReport } from '../types';
 
-import { deleteBugReport, deleteFeedbackReport } from '../api';
+import {
+    deleteBugReport,
+    deleteFeedbackReport,
+    replyToReport,
+    updateReportResolvedStatus,
+} from '../api';
 
 type Report = FeedbackReport | BugReport;
 interface SummaryProps {
@@ -44,17 +54,23 @@ const useStyles = makeStyles((theme: Theme) => ({
     green: {
         color: green[600],
     },
-    bold: {
-        fontWeight: 'bold',
-    },
     marginTop: {
         marginTop: 25,
     },
 }));
 
+// TODO: Auth
+const user = {
+    isAdmin: true,
+};
+
 export default function ReportSummary({ report, callBack }: SummaryProps) {
     const [showReplies, setShowReplies] = React.useState(false);
+    const [showDialog, setShowDialog] = React.useState(false);
+    const [replyContent, setReplyContent] = React.useState('');
+    const [resolvedStatus, setResolvedStatus] = React.useState(report.resolved);
     const [snack] = useSnack();
+    const classes = useStyles();
 
     const handleShowRepliesClick = () => {
         setShowReplies(!showReplies);
@@ -89,38 +105,111 @@ export default function ReportSummary({ report, callBack }: SummaryProps) {
         },
     });
 
-    const classes = useStyles();
-
     const onSuccess = (reportState: Report) => {
         updateReport(reportState);
         callBack();
     };
 
+    const APIDict: { Feedback: 'feedback'; Bug: 'bugs' } = {
+        Feedback: 'feedback',
+        Bug: 'bugs',
+    };
+
+    const replyToAPIRequest = React.useCallback(
+        () =>
+            replyToReport(
+                report._id,
+                replyContent,
+                new Date().toISOString(),
+                APIDict[report.type]
+            ),
+        [report, replyContent]
+    );
+
+    const [sendReplyRequest, isReplyLoading] = useEndpoint(replyToAPIRequest, {
+        onSuccess: () => {
+            setShowDialog(false);
+            snack('Reply successfully submitted', 'success');
+            setReplyContent('');
+        },
+    });
+
+    const sendReply = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        sendReplyRequest();
+    };
+
+    const updateResolvedAPIRequest = React.useCallback(
+        () =>
+            updateReportResolvedStatus(
+                report._id,
+                resolvedStatus,
+                APIDict[report.type]
+            ),
+        [report, resolvedStatus]
+    );
+
+    const [sendUpdateResolvedRequest, isResolvedStatusLoading] = useEndpoint(
+        updateResolvedAPIRequest,
+        {
+            onSuccess: () => {
+                snack('Resolved status successfully updated', 'success');
+            },
+        }
+    );
+    function ResolvedSection() {
+        return user.isAdmin ? (
+            <Grid component='label' container alignItems='center' spacing={2}>
+                {isResolvedStatusLoading ? (
+                    <Loader />
+                ) : (
+                    <Grid item alignItems='center' container spacing={1}>
+                        <Grid item>Unresolved</Grid>
+                        <Grid item>
+                            <Switch
+                                checked={resolvedStatus}
+                                onChange={() => {
+                                    setResolvedStatus(!resolvedStatus);
+                                    sendUpdateResolvedRequest();
+                                }}
+                                name='resolved'
+                            />
+                        </Grid>
+                        <Grid item>Resolved</Grid>
+                    </Grid>
+                )}
+            </Grid>
+        ) : (
+            <Bold>
+                {report.resolved ? (
+                    <>
+                        <ResolvedIcon className={classes.green} />
+                        Your report has been reviewed and used to improve
+                        Prytaneum. Thank you very much!
+                    </>
+                ) : (
+                    <>
+                        <UnresolvedIcon className={classes.yellow} />
+                        Your report is still pending to be reviewed. We will
+                        address it at our earliest convenience. Thank you for
+                        your patience.
+                    </>
+                )}
+            </Bold>
+        );
+    }
+
     return (
         <Grid container spacing={3}>
             <Grid item xs={12}>
-                <Typography variant='h4' align='left'>
-                    Date Submitted:
-                    {formatDate(new Date(report.date))}
-                </Typography>
+                <Bold>
+                    <h1>
+                        {`Date Submitted: ${formatDate(new Date(report.date))}`}
+                    </h1>
+                </Bold>
             </Grid>
             <Grid item xs={12}>
-                <Bold>
-                    {report.resolved ? (
-                        <>
-                            <ResolvedIcon className={classes.green} />
-                            Your report has been reviewed and used to improve
-                            Prytaneum. Thank you very much!
-                        </>
-                    ) : (
-                        <>
-                            <UnresolvedIcon className={classes.yellow} />
-                            Your report is still pending to be reviewed. We will
-                            address it at our earliest convenience. Thank you
-                            for your time.
-                        </>
-                    )}
-                </Bold>
+                <ResolvedSection />
             </Grid>
             <Grid item xs={12}>
                 <Typography variant='body1' paragraph>
@@ -153,6 +242,73 @@ export default function ReportSummary({ report, callBack }: SummaryProps) {
                     }
                 />
             </Grid>
+            {user.isAdmin && (
+                <Grid item container>
+                    <Grid item xs={12}>
+                        <Button
+                            id='showReplies'
+                            fullWidth
+                            variant='contained'
+                            color='primary'
+                            startIcon={<ReplyIcon />}
+                            onClick={() => setShowDialog(true)}
+                        >
+                            Reply
+                        </Button>
+                    </Grid>
+                    <Dialog
+                        open={showDialog}
+                        onClose={() => setShowDialog(false)}
+                    >
+                        <Grid container align-items='center' justify='center'>
+                            <Grid item lg={6} xs={10}>
+                                <form
+                                    onSubmit={sendReply}
+                                    style={{ marginTop: '30vh' }}
+                                >
+                                    <Grid
+                                        container
+                                        align-content='center'
+                                        justify='center'
+                                        spacing={4}
+                                    >
+                                        <Grid item xs={12}>
+                                            <TextField
+                                                id='replyContent'
+                                                required
+                                                multiline
+                                                label='Reply Content'
+                                                value={replyContent}
+                                                onChange={(e) =>
+                                                    setReplyContent(
+                                                        e.target.value
+                                                    )
+                                                }
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <LoadingButton
+                                                loading={isReplyLoading}
+                                                component={
+                                                    <Button
+                                                        type='submit'
+                                                        id='submitReplyButton'
+                                                        variant='contained'
+                                                        fullWidth
+                                                        color='primary'
+                                                    >
+                                                        Submit
+                                                    </Button>
+                                                }
+                                            />
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </Grid>
+                        </Grid>
+                    </Dialog>
+                </Grid>
+            )}
             {report.replies.length > 0 && (
                 <Grid item container>
                     <Grid item xs={12}>
