b207d4a15c0934c0cbe0ce35acc3a3ec72e96813
Johan Guzman <jguzm022@ucr.edu>
2020-09-16 11:14:47 -0700

    Makes new api return unknown date for now

--- src/domains/FeedbackPortal/api/api.ts
+++ src/domains/FeedbackPortal/api/api.ts
@@ -135,7 +135,10 @@ export async function updateReportResolvedStatus(
     const body = {
         resolvedStatus: resolved,
     };
-    return axios.post(`/api/${reportType}/update-resolved-status/${_id}`, body);
+    return axios.post<unknown>(
+        `/api/${reportType}/update-resolved-status/${_id}`,
+        body
+    );
 }
 
 export async function replyToReport(
@@ -154,5 +157,5 @@ export async function replyToReport(
         replyContent,
     };
 
-    return axios.post(`/api/${reportType}/reply-to/${_id}`, body);
+    return axios.post<unknown>(`/api/${reportType}/reply-to/${_id}`, body);
 }
